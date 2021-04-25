2c196e99ffac56fa4373972b7821d0e37b8da4f4
Phillip Schichtel <phillip@schich.tel>
2021-04-25 22:08:42 +0200

    building menu is built now

--- Assets/UI/BuildingMenuController.cs
+++ Assets/UI/BuildingMenuController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using TileGrid;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +8,9 @@ namespace UI
 {
     public class BuildingMenuController : MonoBehaviour
     {
+        public TileGridController tileGridController;
+        public GameObject buildingButtonPrefab;
+        
         private void Start()
         {
             gameObject.SetActive(false);
@@ -13,6 +18,28 @@ namespace UI
 
         public void TileSelected(TileScript tile)
         {
+            for (var i = 0; i < transform.childCount; ++i)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+            var buildableStructures =
+                tile.TechTree.Structures.Where(s => s.texture && s.CanBuild(tileGridController, tile.pos)).ToList();
+
+            if (buildableStructures.Count == 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                foreach (var structure in buildableStructures)
+                {
+                    var obj = Instantiate(buildingButtonPrefab, transform);
+                    obj.name = structure.name;
+                    var rawImage = obj.GetComponent<RawImage>();
+                    rawImage.texture = structure.texture;
+                }
+                gameObject.SetActive(true);
+            }
         }
 
         public void Build(Button button)
