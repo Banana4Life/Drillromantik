e7a7305348244c4c1b4fa7ec97f079ddb828180d
Phillip Schichtel <phillip@schich.tel>
2021-04-25 02:34:47 +0200

    zooming

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,10 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 public class CameraController : MonoBehaviour
 {
     public float PanSpeed = 1f;
+    public float minZoomLevel = 10f;
+    public float maxZoomLevel = 30f;
+    public float startZoomLevel = 20f;
+    public float zoomSpeed = 60f;
     public PostProcessVolume PostProcessVolume;
+
+    private void Start()
+    {
+        var pos = transform.position;
+        transform.position = new Vector3(pos.x, startZoomLevel, pos.z);
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,9 +24,17 @@ public class CameraController : MonoBehaviour
         if (Input.GetButton("Fire2"))
         {
             float dx = Input.GetAxis("Mouse X");
-            float dy = Input.GetAxis("Mouse Y");
+            float dz = Input.GetAxis("Mouse Y");
 
-            transform.Translate(-dx * PanSpeed, 0, -dy * PanSpeed);
+            transform.Translate(-dx * PanSpeed, 0, -dz * PanSpeed);
+        }
+
+        var dy = Input.GetAxis("Mouse ScrollWheel");
+        if (dy != 0f)
+        {
+            var pos = transform.position;
+            pos.y = Mathf.Clamp(pos.y - dy * zoomSpeed, minZoomLevel, maxZoomLevel);
+            transform.position = pos;
         }
           
         var cameraTransform = Camera.main.transform;
